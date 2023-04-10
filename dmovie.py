7eebd8f3972a8da93763c89f465232a46c9e1b94
seungjune Hwang <seungjune.hwang@gmail.com>
2023-04-10 07:28:22 +0000

    힘든 함수였다

--- dmovie.py
+++ dmovie.py
@@ -3,6 +3,10 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
+# 내가 만든 모듈(함수)를 포함
+# import func
+from func import my_print
+
 # 다음 영화 순위 페이지 URL
 url = "https://movie.daum.net/ranking/reservation"
 
@@ -20,12 +24,12 @@ if response.status_code == 200:
     for tr in movie_list:
         rank = rank + 1
         a_tag = tr.select_one("a")
-        print(f'{rank}위 {a_tag.text}')
+        my_print(f'{rank}위 {a_tag.text}')
         txt_grade = tr.select_one("span.txt_grade")
-        print(f'평점: {txt_grade.text}')
+        my_print(f'평점: {txt_grade.text}')
         txt_num = tr.select_one("span.txt_num")
-        print(f'예매율: {txt_num.text}')
+        my_print(f'예매율: {txt_num.text}')
         txt_date = tr.select_one(".txt_info > span.txt_num")
-        print(f'개봉날짜:4 {txt_date.text}')
+        my_print(f'개봉날짜:4 {txt_date.text}')
 else:
     print("HTTP 요청 실패")
